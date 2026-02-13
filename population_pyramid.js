9ab86fbc2b7bd5dabfb7e51b5af1ee43cddade43
yjlei95048 <yjlei2010@yahoo.com.tw>
2026-02-13 15:14:24 +0800

    Add files via upload

--- population_pyramid.js
+++ population_pyramid.js
@@ -1,42 +1,15 @@
 alert("JS已載入");
 
 fetch('population_pyramid.csv')
-.then(response => response.text())
+.then(response => {
+    console.log("Response:", response);
+    return response.text();
+})
 .then(data => {
 
-const rows = data.trim().split(/\r?\n/).slice(1);
+    console.log("CSV內容:", data);
 
-let age = [];
-let male = [];
-let female = [];
-
-rows.forEach(row => {
-    const cols = row.split(/[,;\t]/);
-    console.log(cols);
-
-
-    if(cols.length >= 4 && cols[0].trim() === '大武鄉'){
-
-    if(cols[2].trim() === '男'){
-        age.push(cols[1].trim());
-        male.push(parseInt(cols[3]));
-    } 
-    else if(cols[2].trim() === '女'){
-        female.push(parseInt(cols[3]));
-    }
-}
-});
-
-var chart = echarts.init(document.getElementById('chart'));
-
-chart.setOption({
-    tooltip: { trigger: 'axis' },
-    xAxis: { type: 'value' },
-    yAxis: { type: 'category', data: age.reverse() },
-    series: [
-        { name:'男', type:'bar', data: male.reverse() },
-        { name:'女', type:'bar', data: female.reverse() }
-    ]
-});
+    const rows = data.trim().split(/\r?\n/).slice(1);
+    console.log("Rows數量:", rows.length);
 
 });
