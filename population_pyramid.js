ba9174451be86b0a1925825487a0f76378ccb763
yjlei95048 <yjlei2010@yahoo.com.tw>
2026-02-13 15:19:00 +0800

    Add files via upload

--- population_pyramid.js
+++ population_pyramid.js
@@ -1,15 +1,18 @@
 alert("JS已載入");
 
-fetch('population_pyramid.csv')
-.then(response => {
-    console.log("Response:", response);
-    return response.text();
-})
-.then(data => {
+// 直接寫死一組測試資料
+let age = ["0-4歲","5-9歲","10-14歲"];
+let male = [-71,-89,-79];
+let female = [60,99,97];
 
-    console.log("CSV內容:", data);
+var chart = echarts.init(document.getElementById('chart'));
 
-    const rows = data.trim().split(/\r?\n/).slice(1);
-    console.log("Rows數量:", rows.length);
-
+chart.setOption({
+    tooltip: { trigger: 'axis' },
+    xAxis: { type: 'value' },
+    yAxis: { type: 'category', data: age },
+    series: [
+        { name:'男', type:'bar', data: male },
+        { name:'女', type:'bar', data: female }
+    ]
 });
